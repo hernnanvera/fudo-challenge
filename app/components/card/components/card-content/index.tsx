204d3b113b42947a120dc8299356f0151fffd73d
hernnanvera <v.andinohernan@gmail.com>
2023-01-31 01:39:06 -0300

    add see more anchor button

--- app/components/card/components/card-content/index.tsx
+++ app/components/card/components/card-content/index.tsx
@@ -11,7 +11,10 @@ export default function CardContent({ title, author, description }: CardContentP
             <div className="card__content">
                 <h2>{title}</h2>
                 <p className="card__content__description">{description}</p>
-                {author && <p className="card__content__author">{author}</p>}
+                <p className="card__content__author">{author}</p>
+                <div className="card__content__button">
+                    Ver mas
+                </div>
             </div>
         </>
     )
