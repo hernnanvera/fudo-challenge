6fd53be04386d5581f935620891bd71051a45d5e
hernnanvera <v.andinohernan@gmail.com>
2023-01-31 18:13:28 -0300

    add anchor to header logo

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,16 +1,22 @@
 interface HeaderProps {
     logo?: string
     logoAlt?: string
+    logoTitle?: string
 }
 
-export default function Header({ logo, logoAlt }: HeaderProps): JSX.Element {
+export default function Header({ logo, logoAlt, logoTitle }: HeaderProps): JSX.Element {
     const showLogo = !!logo;
     return (
         <header>
             <div className="header-container">
-                <div className="header-container__logo">
-                    {showLogo && < img src={logo} alt={logo} width={160} height={40} />}
-                </div>
+                {
+                    showLogo &&
+                    <div className="header-container__logo">
+                        <a href="/" title={logoTitle}>
+                            < img src={logo} alt={logoAlt} width={160} height={40} />
+                        </a>
+                    </div>
+                }
             </div>
         </header>
     )
